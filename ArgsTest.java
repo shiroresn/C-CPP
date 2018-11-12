dca09f656365228cac0bbe01f5b80e924a69afab
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-11-12 10:46:32 +0530

    Create ArgsTest.java

---
+++ ArgsTest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class ArgsTest
+{
+public static void main(String args[]) throws Exception
+{
+System.out.println(args[0]);
+
+System.out.println(args[1]);
+
+System.out.println(args[2]);
+
+int a = Integer.parseInt(args[0]);
+
+System.out.println("int a = "+a);
+
+
+}
+}
